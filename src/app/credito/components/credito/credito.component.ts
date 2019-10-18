7641dbc8e2b515734762b125e84659235ff2787a
EtecAB <luizmisslin@hotmail.com>
2019-10-18 14:19:29 -0300

    Adicionado componente e servidor de credito junto com a implementação no front-end

--- src/app/credito/components/credito/credito.component.ts
+++ src/app/credito/components/credito/credito.component.ts
@@ -8,11 +8,36 @@ import { CreditoService } from '../../services';
 })
 export class CreditoComponent implements OnInit {
 
-  constructor(CreditoService: CreditoService) { 
-    
+  constructor(private somarCredito: CreditoService) { 
+    private _valor: number;
   }
 
   ngOnInit() {
+    this._valor = 10;
+  }
+
+  calcula(valorRecebido:number, tipo:string){
+    this._valor = this.credito.somarCredito(this.valor,valorRecebido);
+    if (tipo == "um")
+    {
+      this._valor + 1;
+    }
+    else if(tipo == "dez")
+    {
+      this._valor + 2;
+    }
+    else if(tipo == "cem")
+    {
+      this._valor + 100;
+    }
+    else if(tipo == "descontar")
+    {
+      this._valor - 1;
+    }
+  }
+
+  get pegaValor(){
+    return this._valor
   }
 
 }
